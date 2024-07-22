d05d98574a36a597237297b0e89629ee9f5c9313
Nándor Ónodi <nonodi@cinemo.com>
2024-07-22 11:48:19 +0200

    Add custom parser for proj language

--- nvim_plugin_setup.lua
+++ nvim_plugin_setup.lua
@@ -45,6 +45,16 @@ require("nvim-treesitter.configs").setup {
     },
 }
 
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.proj = {
+    install_info = {
+        url = "~/.local/share/nvim/lazy/tree-sitter-proj",
+        files = {"src/parser.c"},
+        generate_requires_npm = false,
+        requires_generate_from_grammar = false,
+    },
+}
+
 require("telescope").setup {
     defaults = {
         layout_strategy = "vertical",
